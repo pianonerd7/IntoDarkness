789ab0a7f168141da92c450a4d55e8d97cd0da59
pianonerd7 <jxh604@case.edu>
2016-04-12 22:20:22 -0400

    camera follows player

--- Assets/SmoothFollow.cs
+++ Assets/SmoothFollow.cs
@@ -3,22 +3,16 @@ using System.Collections;
 
 public class SmoothFollow : MonoBehaviour {
 
-    public Transform target;
-    public float dampTime = 0.3f;
-    private Transform thisTransform;
-    private Vector3 velocity = new Vector3(1, 1, 1);
-    private Transform mycamera;
+    public GameObject player;
+    private Vector3 offset;
 
     void Start()
     {
-        mycamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        offset = transform.position - player.transform.position;
     }
 
-    void Update()
+    void LateUpdate()
     {
-        //float x = Mathf.SmoothDamp(mycamera.position.x, target.position.x, ref velocity.x, dampTime);
-        //float z = Mathf.SmoothDamp(mycamera.position.z, target.position.z, ref velocity.z, dampTime);
-
-        mycamera.position = new Vector3(target.position.x, 0, target.position.z);
+        transform.position = player.transform.position + offset;
     }
 }
